9114602e7375bfde03490282389dccf4bb918776
chipbaber <chipbaber@yahoo.com>
2019-12-29 12:38:20 -0500

    Basics tested for 15 min. while on break.

---
+++ hello_world.py
@@ -0,0 +1,30 @@
+#This is basic code to walk through the tutorials as I learn python.
+print("Hello Chip")
+
+#Combing Text
+p1 = "This is a"
+p2 = " test of combining text"
+
+print(p1+p2)
+
+#Getting substring
+print(p1[0:4])
+
+#Begining to letter
+print(p1[:3])
+
+#End letter
+print(p1[4:])
+
+#Create a basic list, get length
+
+baseball_numbers = [11, 21, 31, 0, 99]
+print(len(baseball_numbers))
+
+# Basic loop
+print("Starting loop")
+i = 0
+while i<10:
+    print(i)
+    i=i+1
+print("All Done with loop")
